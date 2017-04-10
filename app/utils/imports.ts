6b2711f3fbd70616833c8dd1874967542b5bab94
Alexandre NICOLAIE <Alexandre.Nicolaie@Kratos-ISI.com>
2017-04-10 15:55:42 +0200

    [compil] add rollup stage

--- app/utils/imports.ts
+++ app/utils/imports.ts
@@ -1,14 +1,5 @@
-import _moment_tz_ from 'moment-timezone';
-import _lodash_ from 'lodash';
-import _numeral_ from 'numeral';
-
-let moment: typeof _moment_tz_;
-moment = (_moment_tz_ === undefined) ? require('moment-timezone') : _moment_tz_;
-
-let _: typeof _lodash_;
-_ = (_lodash_ === undefined) ? require('lodash') : _lodash_;
-
-let numeral: typeof _numeral_;
-numeral = (_numeral_ === undefined) ? require('numeral') : _numeral_;
+import * as _ from 'lodash';
+let moment = require('moment-timezone');
+let numeral = require('numeral');
 
 export { moment, _, numeral };
---
+++ rollup-config.js
@@ -0,0 +1,31 @@
+import rollup      from 'rollup'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs';
+import uglify      from 'rollup-plugin-uglify'
+
+export default {
+    entry: 'app/main-aot.js',
+    dest: 'oat/builds/kairos-webui.js', // output a single application bundle
+    sourceMap: true,
+    format: 'iife',
+    onwarn: function(warning) {
+        if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
+        if ( warning.message.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
+        console.warn( warning.message );
+    },
+    plugins: [
+        nodeResolve({jsnext: true, module: true}),
+        commonjs({
+            include: [
+                'node_modules/rxjs/**',
+                'node_modules/logdash/**',
+                'node_modules/moment-timezone/**',
+                'node_modules/ng2-bootstrap/**'
+            ],
+            namedExports: {
+                'node_modules/ng2-bootstrap/ng2-bootstrap.js': [ 'AlertModule', 'ButtonsModule', 'TypeaheadModule', 'TooltipModule', 'DatepickerModule' ]
+            }
+        }),
+        uglify()
+    ]
+}
